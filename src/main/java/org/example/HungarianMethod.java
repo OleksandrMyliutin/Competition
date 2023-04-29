e873acc7ab0b991e251b7c4fdf69c747aa98718c
Mykyta Rudenko <mykyta.rudenko04@gmail.com>
2023-04-29 17:56:13 +0300

    created start realization of calculate method

--- src/main/java/org/example/HungarianMethod.java
+++ src/main/java/org/example/HungarianMethod.java
@@ -1,8 +1,18 @@
 package org.example;
 
 public class HungarianMethod {
+
     static double calculate(double[][] matrix){
         double minCost = 0;
+        double[][] tempMatrix = matrix;
+        for(int i = 0; i < tempMatrix.length; i++)
+        {
+            double minRowElement = findMinRowElement(tempMatrix[i]);
+            for(int j = 0; j < tempMatrix[i].length; j++)
+            {
+                tempMatrix[i][j] -= minRowElement;
+            }
+        }
         return minCost;
     }
 
