af831675bad72baeee9124c172739e54499f5574
Shubertov <pold3634@gmail.com>
2023-04-29 17:27:59 +0300

    Second commit. (Shuber) Created HungarianMethod class.

---
+++ src/main/java/org/example/HungarianMethod.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class HungarianMethod {
+    static double calculate(double[][] matrix){
+        double minCost = 0;
+        return minCost;
+    }
+
+    private static double findMinRowElement(double[] row){
+        double minElement = row[0];
+        return minElement;
+    }
+    private static double findMinColElement(double[] col){
+        double minElement = col[0];
+        return minElement;
+    }
+
+
+
+}
